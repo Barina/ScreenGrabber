5cb5fb37d5524d74e03d848559ebe0a2c3c54201
Barina <roy_barina@hotmail.com>
2010-09-15 21:31:12 +0200

    added Facebook photo uploader

---
+++ ScreenGrabber/FacebookConnect.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ScreenGrabber
+{
+    partial class FacebookUploader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FacebookUploader));
+            this.imagePictureBox = new System.Windows.Forms.PictureBox();
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.uploadBackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.detailsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imagePictureBox
+            // 
+            this.imagePictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.imagePictureBox.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.imagePictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imagePictureBox.Location = new System.Drawing.Point(12, 25);
+            this.imagePictureBox.Name = "imagePictureBox";
+            this.imagePictureBox.Size = new System.Drawing.Size(777, 355);
+            this.imagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imagePictureBox.TabIndex = 0;
+            this.imagePictureBox.TabStop = false;
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.uploadButton.Location = new System.Drawing.Point(696, 55);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(75, 23);
+            this.uploadButton.TabIndex = 1;
+            this.uploadButton.Text = "Upload now";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.uploadButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Image to upload:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(3, 39);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(64, 13);
+            this.statusLabel.TabIndex = 5;
+            this.statusLabel.Text = "Uploading...";
+            this.statusLabel.Visible = false;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(6, 55);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(684, 23);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar.TabIndex = 6;
+            this.progressBar.Visible = false;
+            // 
+            // uploadBackgroundWorker
+            // 
+            this.uploadBackgroundWorker.WorkerReportsProgress = true;
+            this.uploadBackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.uploadBackgroundWorker_DoWork);
+            this.uploadBackgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.uploadBackgroundWorker_ProgressChanged);
+            this.uploadBackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.uploadBackgroundWorker_RunWorkerCompleted);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.detailsLabel);
+            this.groupBox1.Controls.Add(this.uploadButton);
+            this.groupBox1.Controls.Add(this.progressBar);
+            this.groupBox1.Controls.Add(this.statusLabel);
+            this.groupBox1.Location = new System.Drawing.Point(12, 386);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(777, 84);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status";
+            // 
+            // detailsLabel
+            // 
+            this.detailsLabel.AutoSize = true;
+            this.detailsLabel.Location = new System.Drawing.Point(6, 16);
+            this.detailsLabel.Name = "detailsLabel";
+            this.detailsLabel.Size = new System.Drawing.Size(37, 13);
+            this.detailsLabel.TabIndex = 7;
+            this.detailsLabel.Text = "details";
+            // 
+            // FacebookUploader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 482);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imagePictureBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(245, 245);
+            this.Name = "FacebookUploader";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Facebook Uploader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FacebookUploader_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imagePictureBox;
+        private System.Windows.Forms.Button uploadButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.ComponentModel.BackgroundWorker uploadBackgroundWorker;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label detailsLabel;
+
+    }
+}
---
+++ ScreenGrabber/FacebookConnect.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ScreenGrabber.Properties;
+using Facebook;
+using Facebook.Components;
+using Facebook.Exceptions;
+using Facebook.Entity;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace ScreenGrabber
+{
+    public partial class FacebookUploader : Form
+    {
+        #region Components.
+        int[] images; // The IDs of selected images.
+        FileInfo[] files; // Paths of working temp files.
+        DirectoryInfo directoryPath; // Working temp directory.
+        #endregion
+
+        // Properties.
+        public bool isWorking { get; set; }
+
+        public FacebookUploader(Image img, int[] imageIDs)
+        {
+            InitializeComponent();
+            imagePictureBox.Image = img;
+            directoryPath = Directory.CreateDirectory(Application.StartupPath + "\\Temp\\");
+            images = imageIDs;
+            detailsLabel.Text = (images.Length > 1 ? images.Length+" images" : "One image") + " selected to upload to Facebook.";
+            files = new FileInfo[images.Length];
+        }
+        
+        #region Events.
+        private void uploadButton_Click(object sender, EventArgs e)
+        {
+            uploadButton.Enabled = false;
+            statusLabel.Visible = true;
+            progressBar.Value = 0;
+            if ((progressBar.Maximum = images.Length) == 1)
+                progressBar.Style = ProgressBarStyle.Marquee;
+            else
+                progressBar.Style = ProgressBarStyle.Blocks;
+            progressBar.Visible = true;
+            isWorking = true;
+            uploadBackgroundWorker.RunWorkerAsync();
+        }
+
+        private void uploadBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                SnapsDatabaseDataSetTableAdapters.SnapsTableAdapter snaps = new SnapsDatabaseDataSetTableAdapters.SnapsTableAdapter();
+                FacebookService service = Settings.Default.FacebookService;
+                User user = Settings.Default.FacebookUser;
+                Dictionary<string, object> userState = new Dictionary<string, object>();
+                Image img;
+
+                #region Saving image list to disk.
+                userState["OperationID"] = 1;
+                for (int i = 0; i < images.Length; i++)
+                {
+                    img = Snap.byteArrayToImage((byte[])snaps.GetSnap(images[i]));
+                    files[i] = new FileInfo(Program.saveToFile(directoryPath.FullName + "tempimg.jpg",
+                        new Snap(img, ImageFormat.Jpeg, (DateTime)snaps.GetDateBySnapID(images[i]), Settings.Default.AccountID)));
+                    
+                    userState["Image"] = img;
+                    userState["Message"] = "Preparing image '"+files[i].Name+"'. "+"("+(i+1)+"/"+files.Length+")";
+                    uploadBackgroundWorker.ReportProgress(i + 1, userState);
+                }
+                #endregion
+
+                #region Uploading the images to Facebook.
+                userState["OperationID"] = 2;
+                for (int i = 0; i < files.Length; i++)
+                {
+                    img = Snap.byteArrayToImage((byte[])snaps.GetSnap(images[i]));
+                    userState["Image"] = img;
+                    userState["Message"] = "Uploading image '" + files[i].Name + "'. " + "(" + (i + 1) + "/" + files.Length + ")";
+                    uploadBackgroundWorker.ReportProgress(i + 1, userState);
+                    service.UploadPhoto("ScreenGrabber Screenshots", files[i]);
+                }
+                #endregion
+
+                #region Clean up everything.
+                img = null;
+                userState["Image"] = Resources.imageEmpty;
+                userState["OperationID"] = 3;
+                for (int i = 0; i < files.Length; i++)
+                {
+                    userState["Message"] = "Cleaning up.. delete '" + files[i].Name + "'. " + "(" + (i + 1) + "/" + files.Length + ")";
+                    files[i].Delete();
+                }
+
+                deleteFolder(directoryPath.FullName);
+                #endregion
+
+                e.Result = true;
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message + "\n\n" + ex.StackTrace, "Error");
+                e.Result = false;
+            }
+        }
+
+        private void uploadBackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Dictionary<string, object> userState = e.UserState as Dictionary<string, object>;
+
+            switch ((int)userState["OperationID"])
+            {
+                case 1:
+                case 3:
+                    progressBar.Style = ProgressBarStyle.Blocks;
+                    break;
+                case 2:
+                    progressBar.Style = ProgressBarStyle.Marquee;
+                    break;
+            }
+
+            imagePictureBox.Image = (Image)userState["Image"];
+            statusLabel.Text = (string)userState["Message"];
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void uploadBackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            isWorking = false;
+            if ((bool)e.Result)
+                Close();
+            uploadButton.Enabled = true;
+            statusLabel.Visible = false;
+            progressBar.Visible = false;
+        }
+
+        private void FacebookUploader_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (isWorking)
+                if (e.Cancel = !(MessageBox.Show("There are still images to upload.\nConfirm cancelation?", "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                    == System.Windows.Forms.DialogResult.Yes))
+                    deleteFolder(directoryPath.FullName);
+        }
+        #endregion
+
+        // Functions.
+        private static void deleteFolder(string path)
+        {
+            try
+            {
+                foreach (var item in Directory.GetFiles(path))
+                    File.Delete(item);
+                Directory.Delete(path);
+            }
+            catch (DirectoryNotFoundException) { }
+        }
+    }
+}
